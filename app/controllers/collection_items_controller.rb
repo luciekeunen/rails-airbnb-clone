ef38a0457896bcdc814e081dd32c7f06947a4171
Lucie Keunen <lucie@keunen.net>
2017-11-28 19:13:01 +0100

    Created CRUD methods for collection_items controller (without testing because waiting for new routes

--- app/controllers/collection_items_controller.rb
+++ app/controllers/collection_items_controller.rb
@@ -1,22 +1,43 @@
 class CollectionItemsController < ApplicationController
   def index
+    @collection_items = CollectionItem.all
   end
 
   def show
+    @collection_item = CollectionItem.find(params[:id])
   end
 
   def new
+    @collection_item = CollectionItem.new
   end
 
   def create
+    @collection_item = CollectionItem.new(collection_item_params)
+      if @collection_item.save
+        redirect_to .................
+      else
+        render :new
+      end
   end
 
   def edit
+    @collection_item = CollectionItem.find(params[:id])
   end
 
   def update
+    @collection_item = CollectionItem.update(collection_item_params)
+    redirect_to .................
   end
 
   def destroy
+    collection_item = CollectionItem.find(params[:id])
+    collection_item.destroy
+    redirect_to ...............
+  end
+
+  private
+
+  def collection_item_params
+    params.require(:collection_item).permit(:book_id, :description, :price_per_day, :edition, :quality)
   end
 end
