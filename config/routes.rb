67f5deaaaddb4b8e03234eab047c6be306a5d1e2
huang <huangdey973@gmail.com>
2017-11-28 19:43:49 +0100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   resources :profiles, only: [ :show, :new, :create, :edit, :update ]
   resources :collection_items, only: [ :index, :show, :create, :new ] do
     resources :reviews, only: [ :new, :create ]
-    resources :reservations, only: [ :new, :create, :edit, :update, :index]
+    resources :reservations, only: [ :new, :create, :edit, :update]
     resources :messages, only: [ :index, :show, :new, :create]
   end
 
+  get 'reservations/index_borrowed', to: 'reservations#index_borrowed'
+  get 'reservations/index_lended', to: 'reservations#index_lended'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
