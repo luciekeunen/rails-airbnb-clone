f0f5cb352675ccbbf98db1f3954192de90caf4b1
Lucie Keunen <lucie@keunen.net>
2017-11-28 16:31:54 +0100

    Added seeds to create users and their profile + books and collection_items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+20.times do
+  Book.create!(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre)
+end
+
+10.times do
+  user = User.create!(email: Faker::Internet.email, password: "123456")
+  Profile.create!(user_id: user.id, first_name: Faker::Superhero.name, last_name: Faker::Superhero.suffix, description: Faker::Superhero.descriptor)
+end
+
+20.times do
+  collection_item = CollectionItem.new(price_per_day: 50)
+  collection_item.book_id = (1..20).to_a.sample
+  collection_item.profile_id = (1..10).to_a.sample
+  collection_item.save!
+end
