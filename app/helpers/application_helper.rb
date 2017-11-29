ecc7679bbeeffe0f57bc66817d4e5223e0f5adde
Lucie Keunen <lucie@keunen.net>
2017-11-29 17:18:47 +0100

    Improved card partial html and scss, add some basic structure and css to collection_items index and show views and profile show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def image_default(instance, default_image)
+    if instance.photo.url
+      cl_image_path instance.photo.url, crop: :fill
+    else
+      image_path default_image
+    end
+  end
 end
