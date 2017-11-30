2718f61de0a3cb05cb844cbbd071f9f010bf6b7f
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-30 13:58:47 +0100

    reviews finished

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,26 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @collection_item = CollectionItem.find(params[:collection_item_id])
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(params_review)
+    @collection_item = CollectionItem.find(params[:collection_item_id])
+    @user = User.find(current_user.id)
+    @review.profile = @user.profile
+    @review.collection_item = @collection_item
+    if @review.save
+      redirect_to @collection_item
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def params_review
+    params.require(:review).permit(:rating, :content)
   end
 end
