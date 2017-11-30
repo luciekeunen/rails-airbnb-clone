3574b3bb36346dcf2b755cef78dfa1504c1323e3
G VDH <vanderhaegen.gauthier@gmail.com>
2017-11-30 22:03:12 +0100

    test without mounted_as

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,4 @@ class Book < ApplicationRecord
   has_many :collection_items
   has_many :reviews, through: :collection_items
   has_many :profiles, through: :collection_items
-
-  mount_uploader :photo, PhotoUploader
 end
