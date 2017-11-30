a2ba37f7f3e661da3dae446d2ed1bcb5e64ddfab
Lucie Keunen <lucie@keunen.net>
2017-11-30 19:03:39 +0100

    In dashboard, displayed reservations on my books and add possibility to accept or refuse it

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,13 @@
 class DashboardsController < ApplicationController
+  before_action :authenticate_user!
+  # before_action :set_user
+
   def dashboard_main
+    user = User.find(current_user)
+    collection_items = user.profile.collection_items
+    collection_items.each do |collection_item|
+      @reservations = collection_item.reservations
+    end
   end
 
   def inbox
@@ -9,5 +17,11 @@ class DashboardsController < ApplicationController
   end
 
   def my_rentals
+  end
+
+  private
+
+  def set_user
+    user = User.find(current_user)
   end
 end
