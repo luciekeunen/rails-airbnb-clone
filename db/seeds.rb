b256dd177f2856495336d6668828976e8cb28ed6
Lucie Keunen <lucie@keunen.net>
2017-12-01 11:20:54 +0100

    Added 2 reservations for each collection item and added some puts to track the seeding process

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+puts "Destroying previous records..."
+
 Reservation.destroy_all
 Message.destroy_all
 Review.destroy_all
@@ -6,12 +8,20 @@ Book.destroy_all
 Profile.destroy_all
 User.destroy_all
 
+puts "Creating books..."
+
 20.times do
   Book.create!(title: Faker::Book.title, author: Faker::Book.author, genre: Faker::Book.genre)
 end
 
+puts "Creating users and their profile..."
+
+user_count = 0
+
 10.times do
-  user = User.create!(email: Faker::Internet.email, password: "123456")
+  user_count += 1
+  email = "user#{user_count}@gmail.com"
+  user = User.create!(email: email , password: "123456")
   Profile.create!(
     user_id: user.id,
     first_name: Faker::Superhero.name,
@@ -25,16 +35,20 @@ end
   )
 end
 
-20.times do
-  collection_item = CollectionItem.new(price_per_day: 50)
+puts "Creating collection_item with 2 reservations each..."
+
+15.times do
+  collection_item = CollectionItem.new(price_per_day: 5)
   collection_item.book = Book.all.sample
   collection_item.profile = Profile.all.sample
   collection_item.save!
+
+  2.times do
+    reservation = Reservation.new(status: "pending", start_day: Date.today, end_day: Date.tomorrow, final_price: 5)
+    reservation.profile = Profile.all.sample
+    reservation.collection_item = collection_item
+    reservation.save!
+  end
 end
 
-10.times do
-  reservation = Reservation.new(status: "pending", start_day: Date.today, end_day: Date.tomorrow, final_price: 5)
-  reservation.profile = Profile.all.sample
-  reservation.collection_item = CollectionItem.all.sample
-  reservation.save!
-end
+puts "Seeds completed! :)"
